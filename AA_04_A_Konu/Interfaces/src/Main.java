710f7bcc3c3bbd6acb99a040282e461daf1b3042
ArdaCoskun <68504085+ArdaCoskun@users.noreply.github.com>
2021-05-01 23:50:04 +0300

    4. Bölüm Konu
    4. bölüm konular eklendi.

---
+++ AA_04_A_Konu/Interfaces/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Logger[] loggers = {
+				new SmsLogger(),
+				new EmailLogger(),
+				new FileLogger()
+				};
+
+		CustomerManager customerManager = new CustomerManager(loggers);
+		
+		Customer arda = new Customer(1, "Arda", "Coskun");
+		
+		customerManager.add(arda);
+		
+	}
+
+}
